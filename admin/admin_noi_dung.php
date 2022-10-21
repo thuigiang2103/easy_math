b61966b9830c89d8f300a14090aa676b6dc88abe
thuigiang2103 <95474966+thuigiang2103@users.noreply.github.com>
2022-10-22 02:38:49 +0700

    sửa 1
    11

--- admin/admin_noi_dung.php
+++ admin/admin_noi_dung.php
@@ -12,11 +12,10 @@
   <title>Easy-Math</title>
 
   <!-- Custom styles for this template-->
-  <link rel="stylesheet" type="text/css" href="./css/admin_style.css">
+  <link rel="stylesheet" type="text/css" href="../css/admin_style.css">
+  <link rel="stylesheet" type="text/scss" href="../css/admin_style.scss">
 </head>
-<div class="a">
-  <p style="font-weight: bolder;">địt mẹ chó mẩu</p>
-</div>
+
 <body id="page-top">
 
   <!-- Page Wrapper -->
@@ -79,11 +78,6 @@
       </li>
 
     
-
-      <!-- Sidebar Toggler (Sidebar) -->
-      <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-      </div>
 
     </ul>
     <!-- End of Sidebar -->
@@ -160,21 +154,16 @@
   </div>
   <!-- End of Page Wrapper -->
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
-
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery.min.js"></script>
+  <script src="vendor/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="../js/admin.js"></script>
 
 </body>
 
