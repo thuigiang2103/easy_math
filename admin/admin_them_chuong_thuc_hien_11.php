a01e222e98fb7e028a99bded9030fb2a601aa642
thuigiang2103 <95474966+thuigiang2103@users.noreply.github.com>
2022-10-27 17:01:49 +0700

    x

---
+++ admin/admin_them_chuong_thuc_hien_11.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+$ten_chuong = $_POST['txtTenChuong'];
+
+require('../config.php');
+$sql= "INSERT INTO `tbl_chuong_trinh` (`ct_chuong`, `ct_ten_chuong`, `th_id`) VALUES (NULL,
+ '".$ten_chuong."', 'LOP12')";
+
+
+mysqli_query($con, $sql);
+?>
+<script type="text/javascript">
+    window.alert("Cập nhật chương thành công!");
+    window.location.href = "../admin/admin_noi_dung_11.php";
+</script>
+
+ 
